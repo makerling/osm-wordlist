c3af856775c4f1fc6c0e26b9f798b0f99826925b
vanderling <github@vanderling.com>
2018-12-13 09:31:35 +0000

    working code: outputs csv file with lines that differ, last thing left is to run compare on exclusion list

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-
 srcfile1665NT = 'output1665wordlist_short.txt'
 srcfile1819 = 'output1819wordlist_short.txt'
+output = 'output.txt'
+
+with open(output, 'w') as clearfile:
+	print('clearing ' + output + ' file')
+with open(output,'a') as f:
+    f.write('COMPARISON of 1665NT with 1819\n')
+    f.write('Num@Ref@1665 Words@1819 Words@Notes\n')
 
 with open(srcfile1665NT) as file1, open(srcfile1819) as file2:
-    for line_from_file_1_raw, line_from_file_2_raw in zip(file1, file2):
-        #line_from_file_1_strip = line_from_file_1_raw.strip()
-        #line_from_file_2_strip = line_from_file_2_raw.strip()
-        print(line_from_file_1_raw.strip())
-        print(line_from_file_2_raw.strip())
-        print()
+    for i, (line_from_file_1_raw, line_from_file_2_raw) in enumerate(zip(file1, file2)):
 
-        #set1819 = set(line_from_file_1_raw.strip())
-
-        #open1665NT = open(srcfile1665NT, "r")
-        #list1665NTRaw = open1665NT.read()
-        #list1665NT = list1665NTRaw.split()
-
-#different = [x for x in set1819 if not x in list1665NT]
-
-#print(different)
-
-
-
-#with open(srcfile1665NT, 'r') as file1665NT:
-    #filedata1665NT = file1665NT.read()
-    
-#with open(srcfile1819, 'r') as file1819:
-#    for fileline1819 in file1819:
-#        #filelineStrip = fileline().strip()
-#        print('1819', fileline1819.strip())        
-#        with open(srcfile1665NT, 'r') as file1665NT:
-#            for fileline1665NT in file1665NT:
-#                print('1665', fileline1665NT.strip())
+        list1819 = line_from_file_1_raw.split()
+        ref = list1819[0]
+        list1819Pop = list1819.pop(0)
+        set1819 = set(list1819)
+        list1665NT = line_from_file_2_raw.split()
+        list1665NTPop = list1665NT.pop(0)
+        set1665NT = set(list1665NT)
+        
+        different1 = [x for x in list1665NT if not x in set1819]
+        different2 = [x for x in list1819 if not x in set1665NT]
+        
+        different1String = ', '.join(different1)
+        different2String = ', '.join(different2)
+        
+        if different1String != different2String:
+            finalString = str(i)+'@'+str(ref)+'@'+str(different1String.strip())+'@'+str(different2String)+'@\n'
+            with open(output,'a') as f:
+                f.write(finalString)
