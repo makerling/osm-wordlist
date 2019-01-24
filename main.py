e6d4e8679bbbef1a4a8737b0de226be4f3b8b6ec
vanderling <github@vanderling.com>
2019-01-24 06:28:33 +0000

    working solution except for capital in one, lower in other

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import unicodedata
+import re
 
-srcfile1665NT = 'output1665wordlist.txt'
-srcfile1819 = 'output1819wordlist.txt'
+srcfile1665NT = 'output1665wordlist_short.txt'
+srcfile1819 = 'output1819wordlist_short.txt'
 srcfileExclude = 'master_exclude_list_1665-1819.txt'
 output = 'output.txt'
 
@@ -16,39 +17,61 @@ with open(srcfile1665NT) as file1, open(srcfile1819) as file2:
 
         f1 = open(srcfileExclude, "r") 
         file1_raw = f1.read()
-        file1_lowercase = file1_raw.lower()
-        file1_normalized = unicodedata.normalize('NFD',file1_lowercase)
-        shaved = ''.join(c for c in file1_normalized if not unicodedata.combining(c))
-        final = unicodedata.normalize('NFC', shaved)
-        file1_words = final.split()
+        #file1_lowercase = file1_raw.lower()
+        #file1_normalized = unicodedata.normalize('NFD',file1_lowercase)
+        #shaved = ''.join(c for c in file1_normalized if not unicodedata.combining(c))
+        #final = unicodedata.normalize('NFC', shaved)
+        file1_words = file1_raw.split()
         file1_set = set(file1_words)
 
-        line_from_file_1_raw_lowercase = line_from_file_1_raw.lower()
-        line_from_file_1_raw_normalized = unicodedata.normalize('NFD',line_from_file_1_raw_lowercase)
-        shaved = ''.join(c for c in line_from_file_1_raw_normalized if not unicodedata.combining(c))
-        final = unicodedata.normalize('NFC', shaved)        
-        list1819 = final.split()
+        #line_from_file_1_raw_lowercase = line_from_file_1_raw.lower()
+        #line_from_file_1_raw_normalized = unicodedata.normalize('NFD',line_from_file_1_raw_lowercase)
+        #shaved = ''.join(c for c in line_from_file_1_raw_normalized if not unicodedata.combining(c))
+        #final = unicodedata.normalize('NFC', shaved)        
+        list1819 = line_from_file_1_raw.split()
         ref = list1819[0]
         list1819Pop = list1819.pop(0)
         set1819 = set(list1819)
-        line_from_file_2_raw_lowercase = line_from_file_2_raw.lower() 
-        line_from_file_2_raw_normalized = unicodedata.normalize('NFD',line_from_file_2_raw_lowercase) 
-        shaved = ''.join(c for c in line_from_file_2_raw_normalized if not unicodedata.combining(c))
-        final = unicodedata.normalize('NFC', shaved)         
-        list1665NT = final.split()
+        #line_from_file_2_raw_lowercase = line_from_file_2_raw.lower() 
+        #line_from_file_2_raw_normalized = unicodedata.normalize('NFD',line_from_file_2_raw_lowercase) 
+        #shaved = ''.join(c for c in line_from_file_2_raw_normalized if not unicodedata.combining(c))
+        #final = unicodedata.normalize('NFC', shaved)         
+        list1665NT = line_from_file_2_raw.split()
         list1665NTPop = list1665NT.pop(0)
         set1665NT = set(list1665NT)
         
+        print (set1665NT)
+        print (set1819)
+        
         different1beforeExclude = [x for x in list1665NT if not x in set1819]
         different1 = [x for x in different1beforeExclude if not x in file1_set]
+        
         different2beforeExclude = [x for x in list1819 if not x in set1665NT]
         different2 = [x for x in different2beforeExclude if not x in file1_set]
         
         different1String = ', '.join(different1)
+        different1StringStripped = re.sub(r'[A-ZÂÇÎİȮÖÛÜĖĠŞ\-][^,]+?\b', '', different1String)
+        print(different1String)
+        print(different1StringStripped)        
+        print
+        
         different2String = ', '.join(different2)
+        different2StringStripped = re.sub(r'[A-ZÂÇÎİȮÖÛÜĖĠŞ\-][^,]+?\b', '', different2String)        
      
         if different1String != different2String:
-            finalString = str(i)+'@'+str(ref)+'@'+str(different1String.strip())+'@'+str(different2String)+'@\n'
-            with open(output,'a') as f:
-                f.write(finalString)
+            #finalString = str(i)+'@'+str(ref)+'@'+str(different1StringStripped.strip())+'@'+str(different2StringStripped)+'@\n'
+            finalString = str(i)+'@'+str(ref)+'@'+str(different1String.strip())+'@'+str(different2String)+'@\n'            
+            checkContainsAlpha = re.search('[a-z]',finalString)
+            if checkContainsAlpha:
+                checkContainsAlphaStripped = re.sub(r'(, , , )|(@, , )|(@ ,)|(@\ʿ )|(, , @)|(, @)|(@ʿ, )|(@, )|(, , )', '@', finalString)
+                with open(output,'a') as f:
+                    f.write(finalString)
+        
+        print (i)
 print('done')
+
+
+for all words in list that begin with capital letter
+    if not exist in the other project list
+        if exist with case ignore
+            make element lowercase
